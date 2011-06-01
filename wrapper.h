b5dbfea486c7ed62f830532aab4a47140863a1d8
Pang Yan Han <pangyanhan@gmail.com>
2011-06-01 20:45:01 +0800

    Fix missing size_t definition in wrapper.h
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

--- wrapper.h
+++ wrapper.h
@@ -1,6 +1,8 @@
 #ifndef POWAUR_WRAPPER_H
 #define POWAUR_WRAPPER_H
 
+#include <stdlib.h>
+
 void *xcalloc(size_t nmemb, size_t sz);
 void *xmalloc(size_t sz);
 void *xrealloc(void *data, size_t sz);
