892b196a18caf32fbf87212871d8f13d37e479c3
Pang Yan Han <pangyanhan@gmail.com>
2011-06-05 14:32:16 +0800

    Data structure: memlist
    There are certain situations which require heavy use of pointers to a backing
    data store, whose size can only be determined at run time. While linked lists
    can do the job, there is heavy memory usage incurred due to the need to store
    pointers for each node.
    
    Arrays are resizable but we will need to manually keep track of the size of
    the array and possibly realloc it, which is against our goal of having a data
    store which will not have its memory address changed.
    
    The memlist:
    1. Allows us to have a backing data store which will not have its memory
       address changed (no reallocations)
    2. Does all the tracking of size of memory pool and reallocation so we don't
       have to worry about that.
    
    The memlist data structure is essentially a list of memory pools, one chained
    after another. The memory pools in a memlist must store the same data type and
    each pool is a fixed block of memory which is guaranteed to stay in the same
    memory address throughout the duration of the program.
    
    The memlist's behaviour is determined on its initialization and it behaves
    differently when storing pointer data types.
    
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

---
+++ memlist.c
@@ -0,0 +1,76 @@
+#include <string.h>
+
+#include "memlist.h"
+#include "wrapper.h"
+
+struct memlist *memlist_new(unsigned int max_elems, unsigned int elemSz, int free_inner)
+{
+	struct memlist *memlist = xcalloc(1, sizeof(struct memlist));
+	memlist->max_elems = max_elems;
+	memlist->elemSz = elemSz;
+	memlist->free_inner = free_inner;
+	memlist->pool = xcalloc(1, sizeof(struct memlist_node));
+	memlist->pool->data = xcalloc(max_elems, elemSz);
+	return memlist;
+}
+
+void *memlist_add(struct memlist *memlist, void *data)
+{
+	struct memlist_node *curpool = memlist->pool;
+	if (curpool->nr >= memlist->max_elems) {
+		curpool = xcalloc(1, sizeof(struct memlist_node));
+		curpool->data = xcalloc(memlist->max_elems, memlist->elemSz);
+		curpool->next = memlist->pool;
+		memlist->pool = curpool;
+	}
+
+	void *dest = (char *) curpool->data + memlist->elemSz * curpool->nr;
+	memcpy(dest, data, memlist->elemSz);
+	curpool->nr++;
+
+	/* NOTE: free_inner assumes that we are storing pointers here. So we will
+	 * dereference the data when free_inner is set.
+	 */
+	return memlist->free_inner ? *(void **) dest : dest;
+}
+
+static void memlist_free_inner(struct memlist *memlist)
+{
+	struct memlist_node *cur;
+	unsigned int i;
+	while (memlist->pool) {
+		cur = memlist->pool;
+		memlist->pool = memlist->pool->next;
+
+		for (i = 0; i < cur->nr; ++i) {
+			/* Ugly casting but can't be helped.
+			 * Here, the memory pool is storing pointers which need to be freed,
+			 * hence the void ** cast followed by dereference
+			 */
+			free(*(void **) ((char *) cur->data + i * memlist->elemSz));
+		}
+
+		free(cur->data);
+		free(cur);
+	}
+
+	free(memlist);
+}
+
+void memlist_free(struct memlist *memlist)
+{
+	if (memlist->free_inner) {
+		memlist_free_inner(memlist);
+		return;
+	}
+
+	struct memlist_node *cur;
+	while (memlist->pool) {
+		cur = memlist->pool;
+		memlist->pool = memlist->pool->next;
+		free(cur->data);
+		free(cur);
+	}
+
+	free(memlist);
+}
---
+++ memlist.h
@@ -0,0 +1,23 @@
+#ifndef POWAUR_MEMLIST_H
+#define POWAUR_MEMLIST_H
+
+struct memlist_node {
+	void *data;
+	unsigned int nr;
+	struct memlist_node *next;
+};
+
+/* Provides a list of fixed size memory pools */
+struct memlist {
+	struct memlist_node *pool;
+	unsigned int elemSz;
+	unsigned int max_elems;
+	int free_inner;
+};
+
+struct memlist *memlist_new(unsigned int max_elems, unsigned int elemSz, int free_inner);
+/* Returns the final location of data in memlist after addition */
+void *memlist_add(struct memlist *memlist, void *data);
+void memlist_free(struct memlist *memlist);
+
+#endif
